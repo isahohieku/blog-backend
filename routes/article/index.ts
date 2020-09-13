3e2f225e086f6bee0dfa618739016fd3bfe1b55e
Isah Ohieku <isahohieku@gmail.com>
2020-09-13 22:41:49 +0100

    added article route

---
+++ routes/article/index.ts
@@ -0,0 +1,10 @@
+import Route from '../../lib/route';
+import HttpMethod from '../../lib/httpMethod';
+import Auth from '../../controllers/auth';
+
+const ARTICLE_URL = '/api/auth';
+
+const auth: Route[] = [
+];
+
+export default auth;
